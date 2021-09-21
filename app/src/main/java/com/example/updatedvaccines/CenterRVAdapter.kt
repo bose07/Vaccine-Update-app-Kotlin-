2b66712890a9b3b2d20aef1b68a00597f9fe83a0
souhardya bose <bose10.souhardya@gmail.com>
2021-09-21 21:18:37 +0530

    Vaccine update Kotlin app

--- app/src/main/java/com/example/updatedvaccines/CenterRVAdapter.kt
+++ app/src/main/java/com/example/updatedvaccines/CenterRVAdapter.kt
@@ -2,7 +2,7 @@ package com.example.updatedvaccines
 
 import androidx.recyclerview.widget.RecyclerView
 
-class CenterRVAdapter(private val centerList: List<CenterRVModel>) : RecyclerView.Adapter ...............{
+class CenterRVAdapter(private val centerList: List<CenterRVModel>) : RecyclerView.Adapter .................{
 
 
 
